e384143884e05ce45bcd9b9713d4bfce94f5c1a7
Petra Lamborn <oleumpetra@gmail.com>
2017-07-06 21:56:46 +1200

    Sketch out some classes and methods; commenting

--- src/orbitals.cpp
+++ src/orbitals.cpp
@@ -5,12 +5,17 @@ std::pair<double, double> force(Body self, Body other) {
 		// Would rather not cause a divide-by-0
 		return {0, 0};
 	}
+
 	double dx = other.posX - self.posX;
 	double dy = other.posY - self.posY;
+	// 
+	// We could take the square root of this - but then we would just have to square it again!
 	double d2 = dx * dy + dy * dy;
 
-	double f = self.mass * other.mass / d2;
+	//The force, as a scalar
+	double f = G * self.mass * other.mass / d2;
 
+	// The angle of the vector between the two bodies
 	double theta = atan2(dy, dx);
 
 	return {cos(theta) * f, sin(theta) * f};
--- src/orbitals.h
+++ src/orbitals.h
@@ -3,9 +3,21 @@
 
 #include <utility>
 #include <cmath>
+#include <vector>
 
+/// @brief The gravitational constant
 const double G = 6.67428e-11;
 
+/**
+ * @brief Struct for holding a classical, Keplarian orbit
+ */
+struct KeplerParameters {
+
+};
+
+/**
+ * @brief Holds some basic information about a "body"
+ */
 struct Body {
 	double posX;
 	double posY;
@@ -13,8 +25,22 @@ struct Body {
 	double mass;
 	double velX;
 	double velY;
+	bool onRails;
 };
 
+/**
+ * @brief Class that contains a prediction for the future locations of a Body
+ */
+class OrbitalPrediction {
+
+};
+
+/// @brief Calculate the amount of force acting on object self by object other
+///
+/// @param self A not-on-rails Body
+/// @param other An on rails body
+///
+/// @return The 2d force vector of other on self (or vice versa)
 std::pair<double, double> force(Body self, Body other);
 
 #endif
